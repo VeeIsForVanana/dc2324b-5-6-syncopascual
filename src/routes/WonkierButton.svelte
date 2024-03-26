faef803a2de668b2d4bddb8b1c593390879047dc
Marcus Pascual <marcusjoaquim05@gmail.com>
2024-03-26 19:49:17 +0800

    feat: add Container and WonkierButton functionality

--- src/routes/WonkierButton.svelte
+++ src/routes/WonkierButton.svelte
@@ -1,19 +1,47 @@
 <script>
     import Container from "./Container.svelte";
+    export let value = 0;
+    export let content = "FizzBuzz";
+    export let locked = false;
+
+    function handleClick() {
+        value += Math.floor(Math.random() * 10);
+
+        if (value % 15 == 0) {
+            content = "FizzBuzz"
+        }
+        else if (value % 3 == 0) {
+            content = "Fizz"
+        }
+        else if (value % 5 == 0) {
+            content = "Buzz"
+        }
+        else {
+            content = "Default"
+        }
+    }
 </script>
 
 <Container>
-    <div data-testid="text-input" contenteditable />
+    <div data-testid="text-input" contenteditable bind:textContent={ content } />
 
-    <button data-testid="lock-button"> </button>
+    <button data-testid="lock-button" on:click={() => {locked = !locked}}>Lock</button>
 
-    <button data-testid="wonkier-button"> </button>
+    <button data-testid="wonkier-button" on:click={handleClick} disabled={locked} class="{locked ? 'locked' : content}">
+        {#if content == "FizzBuzz"}
+            FizzBuzz {value}
+        {:else if content == "Fizz"}
+            Fizz {value}
+        {:else if content == "Buzz"}
+            Buzz {value}
+        {:else}
+            {value}
+        {/if}
+    </button>
 
-    <!--
-        <p>FizzBuzz!</p> 
-
-        should be rendered when the text inside the wonkier-button starts with FizzBuzz
-    -->
+    {#if content.startsWith("FizzBuzz")}
+        <p>FizzBuzz!</p>
+    {/if}
 </Container>
 
 
